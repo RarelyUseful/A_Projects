08578eff0556af869c9fe845d90a364031310bf5
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-03-02 21:01:10 +0100

    Z5 in progress, working build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+class UserInfo {
+  name: string;
+  email: string;
+  language: string;
+
+  constructor(name: string, email: string, language: string) {
+    this.name = name;
+    this.email = email;
+
+    this.language = language;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'A_Projects';
+  public username: string = '';
+  public useremail: string = '';
+  public translating: boolean = false;
+  public language: string = '';
+  public languages: Array<string> = [
+    'Polish',
+    'French',
+    'Pirate',
+    `Dog. Because i'm blind and my dig will read this for me.`,
+  ];
+  _userBase: Array<UserInfo> = [];
+  public submit(
+    name: string,
+    email: string,
+    transated: boolean,
+    language: string
+  ) {
+    if (transated == true) {
+      this._userBase.push(new UserInfo(name, email, language));
+    }
+    if (transated == false) {
+      this._userBase.push(new UserInfo(name, email, 'English'));
+    }
+    this.username = '';
+    this.useremail = '';
+    this.translating = false;
+    this.language = '';
+  }
 }
